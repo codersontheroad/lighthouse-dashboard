4120acb1a6837084d70a452e839d73b37b5d7e7c
Sacha Nacar <sacha.slushy@gmail.com>
2016-01-31 01:27:25 +0000

    rescue message :rocket:

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,7 +10,34 @@ angular.module('app')
   //   syncObject.$bindTo($scope, "data");
   // })
   .controller("SampleCtrl", function($scope, $firebaseArray, NgMap) {
-    var ref = new Firebase("https://gttrzh5n8tm.firebaseio-demo.com/");
+    var ref = new Firebase("https://sizzling-inferno-4233.firebaseio.com/");
     $scope.messages = $firebaseArray(ref);
-    $scope.count = 0;
+    $scope.toggleColor = function(){
+      var index = this.$index;
+      var target = document.getElementsByClassName("ack")[index];
+      if(target.className=="btn btn-success ack"){
+        target.className="btn btn-danger ack";
+        target.innerHTML = "Acknowledge";
+      }else{
+        target.className="btn btn-success ack";
+        target.innerHTML = "Acknowledged";
+        var myDataRef = new Firebase('https://rescue-lighthouse.firebaseio.com/');
+        myDataRef.push({
+              "text": "Someone's coming!"
+        });
+
+      }
+
+      // $("ack").addClass( "success" );
+      // $("ack").removeClass( "danger" );
+      // $("ack").css("background-color", "#000");
+    }
+    // $scope.remove = function() {
+    //     var id = this.$index + 1;
+    //     var itemRef = new Firebase("https://gttrzh5n8tm.firebaseio-demo.com/");
+    //     itemRef.remove();
+    //     console.log(this.$index);
+    //     // $scope.messages.splice(this.$index, 1);
+    //     ref.remove($scope.messages.this.$index);
+    // };
   });
